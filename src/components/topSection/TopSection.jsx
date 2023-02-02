2961ccb925856cbeecc1f59adf0e67ed4d71be6c
itaim18 <37772742+itaim18@users.noreply.github.com>
2023-02-03 01:00:02 +0200

    volume button added

--- src/components/topSection/TopSection.jsx
+++ src/components/topSection/TopSection.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
-import logo from '../../assets/in3D_tech.png';
-
+import React from "react";
+import styled from "styled-components";
+import logo from "../../assets/in3D_tech.png";
+import bgAudio from "../../assets/audio/bgAudio.mp3";
+const song = new Audio(bgAudio);
 export default function TopSection() {
-  const TopSctionContainer = styled.div`
+  const [play, setPlay] = React.useState(false);
+
+  const TopSectionContainer = styled.div`
     position: absolute;
     width: 100%;
     height: 100%;
@@ -23,18 +26,35 @@ export default function TopSection() {
     background-image: url(${logo});
   `;
 
-  const Slogen = styled.h4`
+  const Slogan = styled.h4`
     margin: 0;
     color: white;
     font-weight: 700;
     font-size: 55px;
     margin-top: 10px;
   `;
+  const Volume = styled.button`
+    position: absolute;
+    top: 20px;
+    right: 10px;
+    font-size: 32px;
+    background-color: transparent;
+    border: none;
+  `;
 
   return (
-    <TopSctionContainer>
+    <TopSectionContainer>
+      <Volume
+        onClick={() => {
+          play ? song.pause() : song.play();
+
+          setPlay((prevState) => !prevState);
+        }}
+      >
+        {play ? "🔊" : "🔈"}
+      </Volume>
       <Logo src={logo} />
-      <Slogen>in3D Tech - Daniel Levi</Slogen>
-    </TopSctionContainer>
+      <Slogan>in3D Tech - Daniel Levi</Slogan>
+    </TopSectionContainer>
   );
 }
